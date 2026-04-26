0674a81ffa43da3ed2c284a904d1b3ce92873737
hiru20 <hirushisandeepanee.gmail.com>
2026-04-26 22:42:07 +0530

    Updated index.ts file

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,22 +1,45 @@
-import { createRouter, createWebHistory } from 'vue-router';
-import HomeView from '../views/HomeView.vue';
-
-const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    component: () => import('../views/AboutView.vue')
-  }
-];
+import { createRouter, createWebHistory } from 'vue-router'
+import { useAuthStore } from '@/stores/authStore'
+import HomeView from '@/views/HomeView.vue'
+import ProductDetailView from '@/views/ProductDetailView.vue'
+import LoginView from '@/views/LoginView.vue'
+import CartView from '@/views/CartView.vue'
 
 const router = createRouter({
-  history: createWebHistory(),
-  routes
-});
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    },
+    {
+      path: '/product/:id',
+      name: 'product-detail',
+      component: ProductDetailView,
+      props: true
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: LoginView
+    },
+    {
+      path: '/cart',
+      name: 'cart',
+      component: CartView
+    }
+  ],
+  scrollBehavior() {
+    return { top: 0 }
+  }
+})
 
-export default router;
+router.beforeEach((to) => {
+  const auth = useAuthStore()
+  if (to.meta.requiresAuth && !auth.isLoggedIn) {
+    return { name: 'login', query: { redirect: to.fullPath } }
+  }
+})
+
+export default router
